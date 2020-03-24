ed95e7cd9ac4459dabfab05ade58c14178b40637
KevenLi <pbleester@gmail.com>
2020-03-24 22:44:05 +0800

    docker entrypoint timeout to 30s

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 python manage.py collectstatic --noinput
-./wait-for-it.sh $DB_HOST:$DB_PORT
+
 if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
-    python manage.py migrate --noinput
+  ./wait-for-it.sh $DB_HOST:$DB_PORT -t 30
+  python manage.py migrate --noinput
 fi
 exec uwsgi imagebank/uwsgi.ini
