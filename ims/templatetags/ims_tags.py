ed6194c0613280a9c68ba9a477e2c261513edf33
KevenLi <pbleester@gmail.com>
2020-03-24 16:58:43 +0800

    user_categories return null if not authenticated.

--- ims/templatetags/ims_tags.py
+++ ims/templatetags/ims_tags.py
@@ -7,6 +7,8 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def user_categories(context):
     user = context.request.user
+    if not user.is_authenticated:
+        return None
     return Category.objects.filter(owner=user)
 
 
