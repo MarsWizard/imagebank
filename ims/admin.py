bd38a2e65e8fe1349e5087cd913d7c9d74a002f0
KevenLi <pbleester@gmail.com>
2020-03-26 15:43:30 +0800

    add searching by title to album admin page

--- ims/admin.py
+++ ims/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 from .models import ImageFile, Image, Category, Album, Tag
 
-# Register your models here.
 
 class ImageAdminView(admin.ModelAdmin):
     list_display = ['album']
 
+
 class AlbumAdminView(admin.ModelAdmin):
     list_display = ['id', 'title', 'owner']
     filter_horizontal = ['tags']
+    search_fields = ['title']
+
 
 admin.site.register(Category)
 admin.site.register(Album, AlbumAdminView)
