889a53c293597f55ab7ac8e25c08ec2aebf96dd1
KevenLi <pbleester@gmail.com>
2020-03-31 16:19:09 +0800

    add flag to auto-create superuser in entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,4 +6,9 @@ if [ "x$DJANGO_MANAGEPY_MIGRATE" = 'xon' ]; then
   ./wait-for-it.sh $DB_HOST:$DB_PORT -t 30
   python manage.py migrate --noinput
 fi
+
+if [ "x$DJANGO_MANAGEPY_INITADMIN" = 'xon' ]; then
+  python manage.py initadmin
+fi
+
 exec uwsgi imagebank/uwsgi.ini
